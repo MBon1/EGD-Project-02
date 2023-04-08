b46e62ec0925ddcb4ef5dd589b11c517e5e77e5d
Matthew Bonnecaze <bonnecaze.matthew@gmail.com>
2023-04-08 14:16:32 -0400

    Done

--- EGD-Project-02/EGD-Project-02/Assets/Script/BGMAudioManager.cs
+++ EGD-Project-02/EGD-Project-02/Assets/Script/BGMAudioManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+// Implementation from https://www.youtube.com/watch?v=PHa5SNe1Mmk
 public class BGMAudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static BGMAudioManager instance;
+    private static AudioClip clip;
+
+    private void Awake()
     {
-        
+        if (instance == null || (instance != this && this.GetComponent<AudioSource>().clip != clip))
+        {
+            if (instance != null)
+            {
+                DestroyInstance();
+                Destroy(instance.gameObject);
+            }
+            instance = this;
+            clip = this.GetComponent<AudioSource>().clip;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void DestroyInstance()
     {
-        
+        if (instance != null)
+            SceneManager.MoveGameObjectToScene(instance.gameObject, SceneManager.GetActiveScene());
     }
 }
